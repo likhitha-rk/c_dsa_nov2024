b4636c030b691393327ed0d89512892993be8c06
likhitha-rk <likhithark7@gmail.com>
2024-11-06 11:30:32 +0530

    Add files via upload

---
+++ nested.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a=4,b=5,c=6;
+    if(a>b){
+        if(a>c)
+        {
+            printf("true");
+        }
+        else {
+            printf("false");
+        }
+
+    }
+    else{
+        printf("invalid");
+    }
+}
---
+++ switch.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int d=3;
+    switch (d)
+    {
+    case 1:
+        printf("monday"); /* code */
+        break;
+        case 2:
+        printf("tuesday");
+        break;
+    default:
+     printf("invalid");
+        break;
+    }
+}
