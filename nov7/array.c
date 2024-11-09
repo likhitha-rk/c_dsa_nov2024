6b3e549cfc1e080f1e0d12efd51c227a2c73c65b
likhitha-rk <likhithark7@gmail.com>
2024-11-09 09:14:17 +0530

    Add files via upload

---
+++ nov7/array.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int arr[]={12,34,36,78,25};
+    printf("%d \n",arr);
+    printf("%d\n",&arr[0]);
+    printf("%d\n",arr[1]);
+    printf("%d\n",arr[2]);
+    printf("%d\n",arr[3]);
+    printf("%d\n",arr[4]);
+}
---
+++ nov8/strings.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    char str[5]="hello";
+    char str2[2]={ 'a','c'};
+    printf("%c\n",str[4]);
+    printf("%c\n",str2[1]);
+}
